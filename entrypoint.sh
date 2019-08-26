b532a604f2b79c16b898e96b877a80a16727340e
zcong1993 <zhangcong1992@gmail.com>
2019-08-26 15:37:16 +0800

    ensure args exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh -l
 
+if [ -z "${INPUT_DINGTOKEN}" ]
+then
+    echo "dingToken is required!"
+    exit 1
+fi
+
+if [ -z "${INPUT_MESSAGE}" ]
+then
+    echo "message is required!"
+    exit 1
+fi
+
 url="https://oapi.dingtalk.com/robot/send?access_token=${INPUT_DINGTOKEN}"
 
 curl "$url" \
